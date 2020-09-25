0bbd1440570f5d44a324cc45c4616e7967648eb7
James Ng <james.ng@projapps.com>
2020-09-25 16:00:53 +0800

    Complete Stage #4

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -11,7 +11,6 @@ iin = "400000"
 def get_checksum(orig_number):
     int_number = [int(number) for number in orig_number]
     mul_number = [n * 2 if i % 2 == 0 else n for i, n in enumerate(int_number)]
-    print(mul_number)
     sub_number = [n - 9 if n > 9 else n for i, n in enumerate(mul_number)]
     total = sum(sub_number)
     return str(((total + 9) // 10 * 10) - total)
@@ -59,7 +58,49 @@ def create_account():
 def print_balance(card_number):
     cur.execute("SELECT * FROM card WHERE number=?", (card_number,))
     row = cur.fetchone()
-    print("Balance: " + row[2])
+    print("Balance: " + str(row[3]))
+    return row[3]
+
+
+def add_income(card_number):
+    print("Enter income:")
+    income = int(input())
+    balance = print_balance(card_number)
+    cur.execute("UPDATE card SET balance=? WHERE number=?", (balance + income, card_number))
+    conn.commit()
+    print("Income was added!")
+    print()
+
+
+def do_transfer(card_number):
+    print("Enter card number:")
+    recv_number = input()
+    if recv_number == card_number:
+        print("You can't transfer money to the same account!")
+    elif get_checksum(recv_number[:-1]) != recv_number[-1]:
+        print("Probably you made a mistake in the card number. Please try again!")
+    elif check_card_number(recv_number) is False:
+        print("Such a card does not exist.")
+    else:
+        print("Enter how much money you want to transfer:")
+        amount = int(input())
+        recv_balance = print_balance(recv_number)
+        card_balance = print_balance(card_number)
+        if amount > card_balance:
+            print("Not enough money!")
+        else:
+            cur.execute("UPDATE card SET balance=? WHERE number=?", (recv_balance + amount, recv_number))
+            cur.execute("UPDATE card SET balance=? WHERE number=?", (card_balance - amount, card_number))
+            conn.commit()
+            print("Success!")
+            print()
+
+
+def close_account(card_number):
+    cur.execute("DELETE FROM card WHERE number=?", (card_number,))
+    conn.commit()
+    print("The account has been closed!")
+    print()
 
 
 def login_account():
@@ -73,7 +114,10 @@ def login_account():
         print()
         while True:
             print("1. Balance")
-            print("2. Log out")
+            print("2. Add income")
+            print("3. Do transfer")
+            print("4. Close account")
+            print("5. Log out")
             print("0. Exit")
             command = int(input())
             print()
@@ -82,6 +126,12 @@ def login_account():
             if command == 1:
                 print_balance(card_number)
             elif command == 2:
+                add_income(card_number)
+            elif command == 3:
+                do_transfer(card_number)
+            elif command == 4:
+                close_account(card_number)
+            elif command == 5:
                 print("You have successfully logged out!")
                 break
     else:
